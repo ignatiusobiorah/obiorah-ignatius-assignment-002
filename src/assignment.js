fdcd954f3ba773ecbe2e54c36b239f87962ab1e7
POPE <59672242+ignatiusobiorah@users.noreply.github.com>
2022-07-15 00:47:05 +0000

    Implemented function for the sumOfNumbers and countOfNumbers

--- src/assignment.js
+++ src/assignment.js
@@ -4,6 +4,8 @@ const assignment = {};
 // ========================
 
 
+
+
 /**
  * Challenge - 1
  * 
@@ -14,9 +16,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    sum = sum + arrayOfNumbers[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countOfNumbers = []
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i] % 2 === 0){
+      //console.log (arrayOfNumbers[i])
+      countOfNumbers.push(arrayOfNumbers[i])
+    }
+  }
+  return countOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
